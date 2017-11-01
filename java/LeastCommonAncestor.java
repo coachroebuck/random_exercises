e1ab8af78c8eb20e148d7420bc3497bfd2f5c77c
Michael Roebuck <mroebuck@cricut.com>
2017-10-31 18:00:08 -0600

    Implemented depth first binary search

--- java/LeastCommonAncestor.java
+++ java/LeastCommonAncestor.java
@@ -51,50 +51,103 @@ public class LeastCommonAncestor {
         if (node == null)
             return -1;
   
-        // If both n1 and n2 are smaller than root, then LCA lies in left
-        if (node.value > n1 && node.value > n2)
-            return lca(node.left, n1, n2);
+  		List<Integer> n1Stack = new ArrayList<Integer>();
+  		List<Integer> n2Stack = new ArrayList<Integer>();
+
+  		final boolean didFindN1 = depthFirstSearch(node, n1, n1Stack);
+        final boolean didFindN2 = depthFirstSearch(node, n2, n2Stack);
+
+        for(Integer i : n1Stack) {
+        	if(n2Stack.contains(i)) {
+        		return i;
+        	}
+        }
+        // // If both n1 and n2 are smaller than root, then LCA lies in left
+        // if (node.value > n1 && node.value > n2)
+        //     return lca(node.left, n1, n2);
   
-        // If both n1 and n2 are greater than root, then LCA lies in right
-        if (node.value < n1 && node.value < n2) 
-            return lca(node.right, n1, n2);
+        // // If both n1 and n2 are greater than root, then LCA lies in right
+        // if (node.value < n1 && node.value < n2) 
+        //     return lca(node.right, n1, n2);
   
-        return node.value;
+        return -1;
     }
 
+    public boolean depthFirstSearch(final Node node, 
+    	final int n,
+    	final List<Integer> stack) {
+    	boolean result = false;
+
+    	if(node != null) {
+	    	if(node.value == n) {
+	    		stack.add(node.value);
+	    		result = true;
+	    	}
+	    	else if(depthFirstSearch(node.left, n, stack)) {
+	    		stack.add(node.value);
+	    		result = true;
+	    	}
+	    	else if(depthFirstSearch(node.right, n, stack)) {
+	    		stack.add(node.value);
+	    		result = true;
+	    	}
+    	}
+
+    	return result;
+    }
+
+	public void print(final List list) {
+		StringBuilder sb = new StringBuilder();
+		for (Object s : list)
+		{
+			if(s instanceof Node) {
+				final Node node = (Node)s;
+				sb.append(node.value);
+			}
+			else {
+				sb.append(s.toString());
+		    }
+		    sb.append("\t");
+		}
+
+		log(sb.toString());
+	}
+
+	public void log(final String message) {
+		System.out.println(message);
+	}
+
     public static void main(String[] args) {
-        final int[] input = /*args.length >= 1 ? getNumericArray(args[0]) :*/
-		 	new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24};
-		LeastCommonAncestor instance = new LeastCommonAncestor(); 
+        LeastCommonAncestor instance = new LeastCommonAncestor(); 
 		List<Node> nodes = new ArrayList<Node>();
 		Node currentNode = null;
-		final int n1 = args.length >= 1 ? Integer.parseInt(args[0]) : 8;
-		final int n2 = args.length >= 2 ? Integer.parseInt(args[1]) : 16;
-    	// instance.add(input);
-    	// System.out.println(instance.lca(input));
-
-    	for(int i = 0; i < input.length; i++) {
+		final int total = args.length >= 1 ? Integer.parseInt(args[0]) : 100;
+		final int n1 = args.length >= 2 ? Integer.parseInt(args[1]) : 12;
+		final int n2 = args.length >= 3 ? Integer.parseInt(args[2]) : 15;
+    	
+    	for(int i = 1; i <= total; i++) {
     		if(currentNode == null) {
-    			instance.root = new LeastCommonAncestor.Node(input[i]);
+    			instance.root = new LeastCommonAncestor.Node(i);
     			currentNode = instance.root;
     		}
     		else if(currentNode.left == null) {
-    			currentNode.left = new LeastCommonAncestor.Node(input[i]);
+    			currentNode.left = new LeastCommonAncestor.Node(i);
     			nodes.add(currentNode.left);
     		}
     		else if(currentNode.right == null) {
-    			currentNode.right = new LeastCommonAncestor.Node(input[i]);	
+    			currentNode.right = new LeastCommonAncestor.Node(i);	
     			nodes.add(currentNode.right);
     		}
     		else {
     			currentNode = nodes.get(0);
     			nodes.remove(0);
-    			currentNode.left = new LeastCommonAncestor.Node(input[i]);
+    			currentNode.left = new LeastCommonAncestor.Node(i);
+    			nodes.add(currentNode.left);
     		}
     	}
     	
-    	System.out.println(instance.total());
-    	System.out.println(instance.isComplete());
+    	System.out.println("total=" + instance.total());
+    	System.out.println("isComplete=" + instance.isComplete());
     	System.out.println(instance.lca(instance.root, n1, n2));
     }
 
